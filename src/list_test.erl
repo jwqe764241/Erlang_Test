2b06d852a8f8a14f24211b8874b6a38e6626c416
jwqe764241 <jwqe764241@gmail.com>
2019-07-26 23:10:13 +0900

    add shorten map in list_test

--- src/list_test.erl
+++ src/list_test.erl
@@ -2,9 +2,12 @@
 
 -export([sum/1]).
 -export([map/2]).
+-export([map2/ 2]).
 
 sum([H | T]) -> H + sum(T);
 sum([]) -> 0.
 
 map(_, []) -> [];
 map(F, [H | T]) -> [F(H) | map (F, T)].
+
+map2(F, L) -> [F(X) || X <- L].
