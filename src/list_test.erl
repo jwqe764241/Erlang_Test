f7a1b799f73934e00986f902f804e3b85170549c
jwqe764241 <jwqe764241@gmail.com>
2019-07-27 00:12:53 +0900

    add pythag, perms

--- src/list_test.erl
+++ src/list_test.erl
@@ -4,6 +4,8 @@
 -export([map/2]).
 -export([map2/ 2]).
 -export([qsort/1]).
+-export([pythag/1]).
+-export([perms/1]).
 
 sum([H | T]) -> H + sum(T);
 sum([]) -> 0.
@@ -18,3 +20,14 @@ qsort([Pivot | T]) ->
     qsort([X || X <- T, X < Pivot])
     ++ [Pivot] ++
     qsort([X || X <- T, X >= Pivot]).
+
+pythag(N) -> [{A, B, C} ||
+    A <- lists:seq(1, N),
+    B <- lists:seq(1, N),
+    C <- lists:seq(1, N), 
+    A + B + C =< N,
+    A*A + B*B =:= C*C
+].
+
+perms([]) -> [[]];
+perms(L) -> [[H | T] || H <- L, T <- perms(L--[H])].
