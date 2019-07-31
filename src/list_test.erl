92c1669b6a08d393fe6adde65782bd9837d6a0b0
jwqe764241 <jwqe764241@gmail.com>
2019-07-31 22:27:03 +0900

    add "case" usage

--- src/list_test.erl
+++ src/list_test.erl
@@ -6,6 +6,8 @@
 -export([qsort/1]).
 -export([pythag/1]).
 -export([perms/1]).
+-export([filter/2]).
+-export([odds_and_evens_acc/1]).
 
 sum([H | T]) -> H + sum(T);
 sum([]) -> 0.
@@ -31,3 +33,19 @@ pythag(N) -> [{A, B, C} ||
 
 perms([]) -> [[]];
 perms(L) -> [[H | T] || H <- L, T <- perms(L--[H])].
+
+filter(P, [H|T]) ->
+    case P(H) of
+        true -> [H | filter(P, T)];
+        false -> filter(P ,T)
+    end;
+filter(P, []) -> [].
+
+odds_and_evens_acc(L) -> odds_and_evens_acc(L, [], []).
+
+odds_and_evens_acc([H|T], Odds, Evens) ->
+    case (H rem 2) of
+        1 -> odds_and_evens_acc(T, [H|Odds], Evens);
+        0 -> odds_and_evens_acc(T, Odds, [H|Evens])
+    end;
+odds_and_evens_acc([], Odds, Evens) -> {lists:reverse(Odds), lists:reverse(Evens)}.
